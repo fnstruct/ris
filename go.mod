e8158cdd182a91326eb654c370c63827a41fea09
fnstruct <184018508+fnstruct@users.noreply.github.com>
2024-10-11 21:35:00 -0400

    Control arguments with command-line flags

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/fnstruct/ris
 
 go 1.23.2
+
+require github.com/itzg/go-flagsfiller v1.14.0
+
+require github.com/iancoleman/strcase v0.3.0 // indirect
--- main.go
+++ main.go
@@ -5,17 +5,44 @@ package main
 
 import (
 	"os"
+	"flag"
 	"fmt"
 	"path/filepath"
+
+	"github.com/itzg/go-flagsfiller"
+)
+
+type Options struct {
+	Name     string `usage:"Specify which files to rename"`
+	Index    int    `usage:"Provide custom index" default:"1"`
+	Version  bool   `usage:"Print installed version"`
+}
+
+var (
+	opts Options
+	Version string
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	err := flagsfiller.Parse(&opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	if opts.Version {
+		fmt.Println(os.Args[0], Version)
 		os.Exit(0)
 	}
 
-	glob := fmt.Sprintf("*.%s", os.Args[1])
-	path := filepath.Join(os.Args[2], glob)
+	args := flag.Args()
+	if len(args) < 1 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	glob := fmt.Sprintf("*.%s", opts.Name)
+	path := filepath.Join(args[0], glob)
 
 	files, err := filepath.Glob(path)
 	if err != nil {
@@ -23,10 +50,12 @@ func main() {
 	}
 
 	for i := 0; i < len(files); i++ {
-		filename := fmt.Sprintf("%0*d.%s", 4, i + 1, os.Args[1])
-		newfile := filepath.Join(os.Args[2], filename)
+		index := fmt.Sprintf("%0*d", 4, i + opts.Index)
+		filename := fmt.Sprintf("%s.%s", index, opts.Name)
 
+		newfile := filepath.Join(args[0], filename)
 		_, err := os.Stat(newfile)
+
 		err = os.Rename(files[i], newfile)
 		if err != nil {
 			panic(err)
